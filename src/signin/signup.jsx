9dee8aa5505972ef1d1111b35190047cc57017fa
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-05-31 17:15:17 +0100

    commit

--- src/signin/signup.jsx
+++ src/signin/signup.jsx
@@ -467,7 +467,7 @@ export default function Signup(params) {
             <select ref={company} id="underline_select" defaultValue={""} className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
               <option value={""} disabled>Male Or Female</option>
               <option value="Male">Male</option>
-              <option value="Fimale">Female</option>
+              <option value="Female">Female</option>
             </select>
           </div>
         </div>
